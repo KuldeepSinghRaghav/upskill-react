cabafc98b60bee86bec9e3e31d7d20dac9ce192f
KuldeepSinghRaghav <kuldeep@studiographene.com>
2025-03-18 15:11:35 +0530

    fix css

--- todo-asignment-03/src/components/Footer.jsx
+++ todo-asignment-03/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Icon from "../../public/icon.svg";
 function Footer() {
   return (
     <div>
-      <div className="footer-border"></div>
+      <div style={{borderTop:'1px solid #e74c3c', marginTop:'172px', width: '1446px'}}></div>
       <div className="footer">
         <div
           className="footer-content"
